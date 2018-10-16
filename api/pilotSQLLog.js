9ca2c4097bd8f860b0f1d8f0b52deb28bb817a9e
lucidph3nx <ph3nx11@gmail.com>
2018-10-16 15:25:16 +1300

    logging enabled

--- api/pilotSQLLog.js
+++ api/pilotSQLLog.js
@@ -5,103 +5,107 @@ module.exports = {
         const Sequelize = require('sequelize');
         let moment = require('moment-timezone');
         moment().tz('Pacific/Auckland').format();
-        let now = moment();
         let currentServices = current.services;
         if (currentServices.length !== 0) {
+            console.log('SQL logging: ' + currentServices.length + ' services');
             for (let s = 0; s < currentServices.length; s++) {
-            return new Promise((resolve, reject) => {
-let rosterQueryString = `
-USE [Test]
-GO
-INSERT INTO [dbo].[pilotLog]
-            ([timeStamp]
-            ,[serviceId]
-            /*,[blockId]
-            ,[line]
-            ,[kiwirail]
-            ,[direction]
-            ,[linkedUnit]
-            ,[cars]
-            ,[speed]
-            ,[locationAgeSeconds]
-            ,[scheduleVariance]
-            ,[varianceFriendly]
-            ,[varianceKiwirail]
-            ,[departs]
-            ,[arrives]
-            ,[origin]
-            ,[destination]
-            ,[lastStation]
-            ,[lastStationCurrent]
-            ,[lastService]
-            ,[hasNextService]
-            ,[nextService]
-            ,[nextServiceTime]
-            ,[LE]
-            ,[LEShift]
-            ,[TM]
-            ,[TMShift]
-            ,[statusMessage]
-            ,[status1]
-            ,[status2]
-            ,[status3]
-            ,[lat]
-            ,[long]
-            ,[meterage]*/)
-        VALUES
-            '`+now+`'
-            ,'`+currentServices[s].serviceId+`'
-            /*,<blockId, varchar(4),>
-            ,<line, varchar(3),>
-            ,<kiwirail, bit,>
-            ,<direction, varchar(4),>
-            ,<linkedUnit, varchar(6),>
-            ,<cars, varchar(5),>
-            ,<speed, real,>
-            ,<locationAgeSeconds, bigint,>
-            ,<scheduleVariance, datetime,>
-            ,<varianceFriendly, int,>
-            ,<varianceKiwirail, int,>
-            ,<departs, datetime,>
-            ,<arrives, datetime,>
-            ,<origin, varchar(4),>
-            ,<destination, varchar(4),>
-            ,<lastStation, varchar(4),>
-            ,<lastStationCurrent, bit,>
-            ,<lastService, varchar(4),>
-            ,<hasNextService, bit,>
-            ,<nextService, varchar(4),>
-            ,<nextServiceTime, datetime,>
-            ,<LE, varchar(50),>
-            ,<LEShift, varchar(50),>
-            ,<TM, varchar(50),>
-            ,<TMShift, varchar(50),>
-            ,<statusMessage, varchar(50),>
-            ,<status1, varchar(50),>
-            ,<status2, varchar(50),>
-            ,<status3, varchar(50),>
-            ,<lat, float,>
-            ,<long, float,>
-            ,<meterage, float,>*/)
-GO
-`;
-            let sequelize = new Sequelize('TEST', 'PilotSQL', 'test', {
-                logging: false,
-                host: '(localdb)\\MyInstance',
-                dialect: 'mssql',
-                dialectOptions: {
-                instanceName: 'LOCALDB#B2E5B83E',
-                options: {
-                    encrypt: false,
-                },
-                },
-            });
-            sequelize.query(rosterQueryString)
-                .then(function(response) {
-                }
-            );
-            });
+                sqlLogThisService(currentServices[s]);
             }
         }
+        /**
+         * loggs a service snapshot to sql db
+         * @param {object} currentService
+         * @return {string} except it doesnt really
+         */
+        function sqlLogThisService(currentService) {
+            let now = moment();
+            return new Promise((resolve, reject) => {
+                let kiwirail;
+                let lastStationCurrent;
+                if (currentService.kiwirail) {kiwirail=1;} else {kiwirail=0;}
+                if (currentService.lastStationCurrent) {lastStationCurrent=1;} else {lastStationCurrent=0;}
+                let rosterQueryString = `
+                INSERT INTO [dbo].[pilotLog]
+                        ([timeStamp]
+                        ,[serviceId]
+                        ,[blockId]
+                        ,[line]
+                        ,[kiwirail]
+                        ,[direction]
+                        ,[linkedUnit]
+                        ,[cars]
+                        ,[speed]
+                        ,[locationAgeSeconds]
+                        ,[scheduleVariance]
+                        ,[varianceFriendly]
+                        ,[varianceKiwirail]
+                        ,[departs]
+                        ,[arrives]
+                        ,[origin]
+                        ,[destination]
+                        ,[lastStation]
+                        ,[lastStationCurrent]
+                        ,[LE]
+                        ,[LEShift]
+                        ,[TM]
+                        ,[TMShift]
+                        ,[statusMessage]
+                        ,[status1]
+                        ,[status2]
+                        ,[status3]
+                        ,[lat]
+                        ,[long]
+                        ,[meterage])
+                    VALUES (
+                        '`+now.format('YYYY-MM-DD HH:mm:ss')+`'
+                        ,'`+currentService.serviceId+`'
+                        ,'`+currentService.blockId+`'
+                        ,'`+currentService.line+`'
+                        ,`+kiwirail+`
+                        ,'`+currentService.direction+`'
+                        ,'`+currentService.linkedUnit+`'
+                        ,'`+currentService.cars+`'
+                        ,`+currentService.speed+`
+                        ,`+currentService.locationAgeSeconds+`
+                        ,'`+currentService.scheduleVariance+`'
+                        ,`+currentService.varianceFriendly+`
+                        ,`+currentService.varianceKiwirail+`
+                        ,'`+currentService.departs+`'
+                        ,'`+currentService.arrives+`'
+                        ,'`+currentService.origin+`'
+                        ,'`+currentService.destination+`'
+                        ,'`+currentService.lastStation+`'
+                        ,`+lastStationCurrent+`
+                        ,'`+currentService.LE+`'
+                        ,'`+currentService.LEShift+`'
+                        ,'`+currentService.TM+`'
+                        ,'`+currentService.TMShift+`'
+                        ,'`+currentService.statusMessage+`'
+                        ,'`+currentService.statusArray[0]+`'
+                        ,'`+currentService.statusArray[1]+`'
+                        ,'`+currentService.statusArray[2]+`'
+                        ,`+currentService.lat+`
+                        ,`+currentService.long+`
+                        ,`+currentService.meterage+`)
+                `;
+                        let sequelize = new Sequelize({
+                            database: 'PILOTSQL',
+                            username: 'PilotLoggingService',
+                            password: 'test',
+                            logging: false,
+                            host: 'D-3VZ5CD2',
+                            port: 1433,
+                            dialect: 'mssql',
+                            options: {
+                                encrypt: false,
+                            },
+                        });
+                        //console.log(rosterQueryString);
+                        sequelize.query(rosterQueryString)
+                            .then(function(response) {
+                            }
+                        );
+                        });
+        };
     },
 };
